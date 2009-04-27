6c7503a6dd2ea89f8de78a1b556bef7e94eb0113
Gareth Andrew <gingerhendrix@hotmail.com>
2009-04-28 00:53:31 +0100

    Basic Styling

---
+++ wordpress/wp-content/themes/carrington-jam-1.1/content/content-default.php
@@ -0,0 +1,51 @@
+<?php
+
+// This file is part of the Carrington JAM Theme for WordPress
+// http://carringtontheme.com
+//
+// Copyright (c) 2008-2009 Crowd Favorite, Ltd. All rights reserved.
+// http://crowdfavorite.com
+//
+// Released under the GPL license
+// http://www.opensource.org/licenses/gpl-license.php
+//
+// **********************************************************************
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// **********************************************************************
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+global $previousday, $authordata;
+
+// Causes the_date to display every time, instead of just once per day.
+$previousday = -1;
+
+?>
+<div>
+
+<a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+
+<?php
+
+the_content(); 
+
+link_pages();
+
+the_date();
+
+echo get_the_author();
+
+the_category(', ');
+
+the_tags(__('Tagged with ', 'carrington-jam'), ', ', '');
+
+comments_popup_link(__('No comments', 'carrington-jam'), __('1 comment', 'carrington-jam'), __('% comments', 'carrington-jam'));
+
+edit_post_link(__('Edit This', 'carrington-jam'), '', '');
+
+?>
+
+</div>
